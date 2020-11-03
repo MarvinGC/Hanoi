17aad08307775396d73b99c42ed5e933f5581cf8
smajkil <55425861+smajkil@users.noreply.github.com>
2020-11-03 11:35:30 -0600

    Update Torre-Hanoi-Recursivo.cpp

--- Torre-Hanoi-Recursivo.cpp
+++ Torre-Hanoi-Recursivo.cpp
@@ -6,24 +6,25 @@ using namespace std;
 
 vector<vector<int>> t;
 
-void resol(int ini, int fin, int plat, int torre, int band)
+void resol(int ini, int fin, int plat,int tPartida, int tObjetivo, int band)
 {
 	int i = 0;
 	int med = (ini+fin)/2;
 	t[med-1][0] = plat;
-	t[med-1][1] = torre;
+	t[med-1][1] = tPartida;
+	t[med-1][2] = tObjetivo;
+
+	(plat%2 == band) ? i = -1 :	i = 1;
+
+	int tOb = (tObjetivo+i)%3;
+	if(tOb == 0) tOb = 3;
+
+	int tPr = (tPartida-i)%3;
+	if(tPr == 0) tPr = 3;
+
 	if(plat > 1){
-		resol(med+1, fin, plat-1, torre,band);
-	
-		if(plat%2 == band)
-			i = -1;
-		else
-			i = 1;
-		torre = (torre+i)%3;
-		if(torre == 0)
-			torre = 3;
-		
-		resol(ini, med, plat-1, torre,band);  
+		resol(med+1, fin, plat-1,tPr, tObjetivo,band);
+		resol(ini, med, plat-1, tPartida, tOb,band);  
 	}	
 }
 int main()
@@ -35,19 +36,20 @@ int main()
 	nMov = pow(2,plat);
 
 	for(int i=0;i<nMov;i++){
-		vector<int> e{0,0};
+		vector<int> e{0,0,0};
 		t.push_back(e);
 	}
 	
 	if(plat%2==0)
-		resol(0,nMov,plat,3,0);
+		resol(0,nMov,plat,1,3,0);
 	else
-		resol(0,nMov,plat,3,1);
+		resol(0,nMov,plat,1,3,1);
 	
 	for(int i = 0; i<nMov-1; i++)
 	{
-		cout<<"Mover Platillo "<<t[i][0]<<" ";
-		cout<<"a torre "<<t[i][1]<<" "<<endl;
+		cout << "Mover Platillo " << t[i][0] << " ";
+		cout << "de torre " << t[i][1] << " ";
+		cout << "a torre " << t[i][2] << " " << endl;
 	}
 	cout<<"\nnumero de movimientos "<<nMov-1<<endl;
 	return 0;
